b5f3fdc29a748a10057872beecfa0b59efb4a6c7
Aswin-A-Arun <57713995+Aswin-A-Arun@users.noreply.github.com>
2019-12-01 17:23:58 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,8 @@
+class Log extends BaseClass {
+    constructor(x, y, height, angle) {
+      super(x,y,20,height,angle);
+      this.image = loadImage("d:\sprites\wood2.png");
+    }
+    
+  };
+    
